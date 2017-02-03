aebc0598a240da0c88f620ebfcf20e40f57cf883
Aaron Ang <awz.ang@gmail.com>
2017-02-03 11:15:59 -0800

    Provide link for next page

--- server.js
+++ server.js
@@ -6,22 +6,33 @@ var port = process.env.PORT || 8080
 var app = express()
 
 mongoose.connect(process.env.MONGODB_URI)
+mongoose.Promise = global.Promise
 
 var router = express.Router()
 router.route('/techs')
   .get((req, res) => {
-    Tech.find((err, techs) => {
-      if (err)
-        res.send(err)
+    console.log('[GET /techs]', req.query)
+    var fullUrl = req.protocol + '://' + req.get('host') + '/api' + req.path;
+    var start =  req.query.start && { $lt: new Date(req.query.start) }
+    var query = {
+      pub_date: start || { $lt: Date.now() }
+    }
 
-      var response = {
-        data: {
-          techs: techs
+    Tech.find(query)
+      .limit(10)
+      .sort('-pub_date')
+      .exec((err, techs) => {
+        if (err)
+          res.send(err)
+
+        var response = {
+          data: {
+            next: fullUrl + '?' + 'start=' + new Date(techs[techs.length - 1].pub_date).toJSON(),
+            techs: techs
+          }
         }
-      }
-
-      res.json(response)
-    })
+        res.json(response)
+      })
   })
 
 app.use('/api', router)
